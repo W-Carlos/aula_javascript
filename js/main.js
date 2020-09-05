8cad193556fe87a6a49e63a31c69add2f511a88b
W.Carlos <wendcarlos18@hotmail.com>
2020-09-05 17:36:20 -0300

    primeira aula

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var nome = "Carlos Nascimento";
+var idade = 24;
+var idade2 = 10;
+var soma = idade + idade2;
+var frase = "Japão é o melhor time do mundo"
+//window.alert(nome + " tem " + idade + " anos");
+//window.alert(soma);
+console.log(nome)
+console.log(idade + idade2)
+console.log(frase.toLowerCase())
+//window.alert(frase.replace("Japão", "Brasil"))
