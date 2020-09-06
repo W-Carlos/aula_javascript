17fadf1fc6b7babb5a6642f67a9f942b6b4d988b
W.Carlos <wendcarlos18@hotmail.com>
2020-09-06 10:48:11 -0300

    segunda aula

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
-var nome = "Carlos Nascimento";
+
+//Dicionario
+
+var frutas = [{nome: "maçã", cor: "vermelha"},{nome: "uva", cor: "roxa"}]
+console.log(frutas);
+window.alert(frutas[1].nome);
+
+/*var fruta = {nome:"maçã", cor:"vermelha"}
+console.log(fruta)
+console.log(fruta.nome)
+window.alert(fruta.cor)*/
+
+/*Array
+var lista = ["maçã", "pêra", "laranja"];
+lista.push("uva");
+//lista.pop()
+console.log(lista)
+//console.log(lista[0])
+//console.log(lista.length);
+//console.log(lista.reverse())
+console.log(lista.toString())
+console.log(lista.join(" - "))*/
+
+/*var nome = "João Nascimento";
 var idade = 24;
 var idade2 = 10;
 var soma = idade + idade2;
 var frase = "Japão é o melhor time do mundo"
-//window.alert(nome + " tem " + idade + " anos");
-//window.alert(soma);
+window.alert(nome + " tem " + idade + " anos");
+window.alert(soma);
 console.log(nome)
 console.log(idade + idade2)
 console.log(frase.toLowerCase())
-//window.alert(frase.replace("Japão", "Brasil"))
+window.alert(frase.replace("Japão", "Brasil"))*/
